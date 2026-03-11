14fc44fb4b962f47f0973160620afe61225d3e0a
Hubert Lefevre <hubelefevre@gmail.com>
2026-03-11 14:01:33 +0100

    cli status: Make struct take the whole SnapshotStats
    Allows to just re-used the existing struct, instead of recreating an
    augmented one. The SnapshotStats is moved anyway.

--- cli/src/commands/status.rs
+++ cli/src/commands/status.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::collections::BTreeMap;
-
 use futures::TryStreamExt as _;
 use itertools::Itertools as _;
 use jj_lib::commit::Commit;
@@ -23,11 +21,9 @@ use jj_lib::merge::Diff;
 use jj_lib::merged_tree::MergedTree;
 use jj_lib::repo::Repo;
 use jj_lib::repo_path::RepoPath;
-use jj_lib::repo_path::RepoPathBuf;
 use jj_lib::revset::RevsetExpression;
 use jj_lib::revset::RevsetFilterPredicate;
 use jj_lib::working_copy::SnapshotStats;
-use jj_lib::working_copy::UntrackedReason;
 use tracing::instrument;
 
 use crate::cli_util::CommandHelper;
@@ -254,7 +250,7 @@ struct WorkingCopyStatus {
     parents: Vec<Commit>,
     parent_tree: MergedTree,
     tree: MergedTree,
-    untracked_paths: BTreeMap<RepoPathBuf, UntrackedReason>,
+    snapshot_stats: SnapshotStats,
 }
 
 impl WorkingCopyStatus {
@@ -263,11 +259,12 @@ impl WorkingCopyStatus {
     }
 
     fn has_any_untracked_paths(&self) -> bool {
-        !self.untracked_paths.is_empty()
+        !self.snapshot_stats.untracked_paths.is_empty()
     }
 
     fn untracked_paths_matching(&self, matcher: &dyn Matcher) -> impl Iterator<Item = &RepoPath> {
-        self.untracked_paths
+        self.snapshot_stats
+            .untracked_paths
             .keys()
             .filter(|path| matcher.matches(path))
             .map(|path| path.as_ref())
@@ -283,13 +280,12 @@ async fn collect_working_copy_status(
     let parents = commit.parents().await?;
     let parent_tree = commit.parent_tree(repo).await?;
     let tree = commit.tree();
-    let untracked_paths = snapshot_stats.untracked_paths;
 
     Ok(WorkingCopyStatus {
         commit,
         parents,
         parent_tree,
         tree,
-        untracked_paths,
+        snapshot_stats,
     })
 }
